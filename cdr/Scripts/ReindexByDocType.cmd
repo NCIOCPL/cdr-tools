b1cd0ba6b3b2aca953628001b358f2dfe31865a8
Bob Kline <bkline@rksystems.com>
2002-02-15 21:42:44 +0000

    <R1315> Moved to cdr/Utilities.

--- cdr/Scripts/ReindexByDocType.cmd
+++
@@ -1,7 +0,0 @@
-if "%1x" == "x" goto usage
-perl perl CdrQuery.pl rmk ***REDACTED*** "CdrCtl/DocType = '%1'" | CdrCmd  | SabCmd filters/ReindexDocs.xsl | CdrCmd >> output\ReindexByDocType.out
-less output/ReindexByDocType.out
-goto out
-:usage
-echo usage: ReindexByDocType DOCTYPENAME
-:out
