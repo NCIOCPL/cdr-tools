ebc287a561c64b361894376cfed7e94ca24c653b
Bob Kline <bkline@rksystems.com>
2002-09-15 19:08:04 +0000

    <R2536> New tools.

---
+++ cdr/Utilities/DenormalizeDocs.py
@@ -0,0 +1,93 @@
+#----------------------------------------------------------------------
+#
+# $Id: DenormalizeDocs.py,v 1.1 2002-09-15 19:08:04 bkline Exp $
+#
+# $Log: not supported by cvs2svn $
+#----------------------------------------------------------------------
+import sys, cdr, cdrdb
+
+filters = {
+    "InScopeProtocol": [
+        "name:Denormalization Filter (1/1): InScope Protocol",
+        "name:Denormalization: sort OrganizationName for Postal Address",
+    ],
+    "Organization": [
+        "name:Denormalization Filter (1/1): Organization",
+        "name:Denormalization: sort OrganizationName for Postal Address",
+    ],
+    "Person": [
+        "name:Denormalization Filter (1/1): Person",
+        "name:Denormalization: sort OrganizationName for Postal Address",
+    ],
+    "Term": [
+        "name:Denormalization Filter (1/1): Terminology",
+    ],
+    "Summary": [
+        "name:Denormalization Filter (1/5): Summary",
+        "name:Denormalization Filter (2/5): Summary",
+        "name:Denormalization Filter (3/5): Summary",
+        "name:Denormalization Filter (4/5): Summary",
+        "name:Denormalization Filter (5/5): Summary",
+    ],
+    "PoliticalSubUnit": [
+        "name:Denormalization Filter (1/1): Political SubUnit",
+        "name:Vendor Filter: PoliticalSubUnit"
+    ],
+}
+
+maxDocs = -1
+rows    = []
+if len(sys.argv) < 2:
+    sys.stderr.write("usage: DenormalizeDocs cdrType [max-docs]\n")
+    sys.stderr.write("   or: DenormalizeDocs cdrType --list id [id ...]\n")
+    sys.stderr.write("   or: DenormalizeDocs --filelist filename\n")
+    sys.exit(1)
+if sys.argv[1] == "--filelist":
+    listFile = open(sys.argv[2])
+    for line in listFile.readlines():
+        line = line.strip()
+        (id, docType) = line.split("\t")
+        rows.append([int(id), docType])
+else:
+    cdrType = sys.argv[1]
+    if not filters.has_key(cdrType):
+        sys.stderr.write("don't know how to filter %s documents\n" % cdrType)
+        sys.exit(1)
+if len(sys.argv) > 2:
+    if sys.argv[2] == '--list':
+        for i in range(3, len(sys.argv)):
+            rows.append([int(sys.argv[i]), cdrType])
+    else:
+        maxDocs = int(sys.argv[2])
+if not rows:
+    conn = cdrdb.connect()
+    curs = conn.cursor()
+    curs.execute("""\
+    SELECT document.id, doc_type.name
+      FROM document
+      JOIN doc_type
+        ON doc_type.id = document.doc_type
+     WHERE doc_type.name = '%s'
+       AND document.active_status = 'A'
+  ORDER BY document.id""" % cdrType)
+    rows = curs.fetchall()
+sess = cdr.login('rmk', '***REDACTED***')
+if maxDocs == -1: maxDocs = len(rows)
+sys.stderr.write("found %d documents; processing %d\n" % (len(rows), maxDocs))
+                 
+numDocs = 0
+for row in rows:
+    if numDocs >= maxDocs:
+        break
+    numDocs += 1
+    id = row[0]
+    cdrType = row[1]
+    if not filters.has_key(cdrType):
+        sys.stderr.write("don't know how to filter %s documents\n" % cdrType)
+        continue
+    resp = cdr.filterDoc('guest', filters[cdrType], id)
+    if type(resp) in (type(""), type(u"")):
+        sys.stderr.write("Error for document %d: %s\n" % (id, resp))
+    else:
+        sys.stderr.write("Processing document CDR%010d\n" % id)
+        open("%d.xml" % id, "w").write(resp[0])
---
+++ cdr/Utilities/UnlockDocsForUser.py
@@ -0,0 +1,37 @@
+#----------------------------------------------------------------------
+#
+# $Id: UnlockDocsForUser.py,v 1.1 2002-09-15 19:08:04 bkline Exp $
+#
+# Unlocks all documents checked out by a user.
+#
+# usage: UnlockDocsForUser user-id password
+#
+# $Log: not supported by cvs2svn $
+#----------------------------------------------------------------------
+import cdr, cdrdb, sys
+
+if len(sys.argv) != 3:
+    sys.stderr.write("usage: UnlockDocsForUser user-id password\n")
+    sys.exit(1)
+conn = cdrdb.connect('CdrGuest')
+cursor = conn.cursor()
+cursor.execute("""\
+        SELECT c.id
+          FROM checkout c
+          JOIN usr u
+            ON u.id = c.usr
+         WHERE u.name = ?
+           AND c.dt_in IS NULL""", sys.argv[1])
+rows = cursor.fetchall()
+print "unlocking %d documents" % len(rows)
+if rows:
+    session = cdr.login(sys.argv[1], sys.argv[2])
+    if session.find("<Err") != -1:
+        sys.stderr.write("Failure logging in to CDR: %s" % session)
+        sys.exit(1)
+    for row in rows:
+        err = cdr.unlock(session, "CDR%010d" % row[0])
+        if err: 
+            sys.stderr.write("Failure unlocking CDR%010d: %s" % (row[0], err))
+        else:
+            print "unlocked CDR%010d" % row[0]
