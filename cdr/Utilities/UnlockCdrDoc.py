a46ca34f893b178e7ba776b98cd7576fd9993b53
Bob Kline <bkline@rksystems.com>
2004-01-10 14:48:03 +0000

    <R4655> Script to unlock a document by its doc id.

---
+++ cdr/Utilities/UnlockCdrDoc.py
@@ -0,0 +1,28 @@
+#----------------------------------------------------------------------
+#
+# $Id: UnlockCdrDoc.py,v 1.1 2004-01-10 14:48:03 bkline Exp $
+#
+# Unlock a document by its doc id.
+#
+# $Log: not supported by cvs2svn $
+#
+#----------------------------------------------------------------------
+import cdr, sys
+
+if len(sys.argv) != 4:
+    sys.stderr.write("usage: UnlockCdrDoc userId pw docId\n")
+    sys.exit(1)
+
+session = cdr.login(sys.argv[1], sys.argv[2])
+if session.find("<Err") != -1:
+    sys.stderr.write("Failure logging in to CDR: %s" % session)
+    sys.exit(1)
+
+# Accept doc id in any form, normalize to CDR000... form
+docId = cdr.exNormalize(sys.argv[3])[0]
+
+err = cdr.unlock(session, docId)
+if err:
+    sys.stderr.write("Failure unlocking %s: %s" % (docId, err))
+else:
+    print "unlocked " + docId
