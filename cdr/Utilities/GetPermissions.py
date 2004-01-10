9dab97ffc044810d03bdc68d627437be43b3198b
Bob Kline <bkline@rksystems.com>
2004-01-10 14:49:19 +0000

    <R4656> Generate a list of who can do what in the CDR.

---
+++ cdr/Utilities/GetPermissions.py
@@ -0,0 +1,31 @@
+#======================================================================
+#
+# $Id: GetPermissions.py,v 1.1 2004-01-10 14:49:19 bkline Exp $
+#
+# Fetch a list of who can do what.
+#
+# $Log: not supported by cvs2svn $
+#======================================================================
+
+import cdrdb, sys
+pattern = len(sys.argv) > 1 and sys.argv[1] or '%'
+host    = len(sys.argv) > 2 and sys.argv[2] or 'localhost'
+conn    = cdrdb.connect(dataSource = host)
+cursor  = conn.cursor()
+cursor.execute("""\
+    SELECT DISTINCT a.name ActionName,
+                    g.name GroupName,
+                    u.name UserName
+               FROM usr u
+               JOIN grp_usr gu
+                 ON gu.usr = u.id
+               JOIN grp g
+                 ON g.id = gu.grp
+               JOIN grp_action ga
+                 ON ga.grp = g.id
+               JOIN action a
+                 ON a.id = ga.action
+              WHERE a.name LIKE '%s'
+           ORDER BY ActionName, GroupName, UserName""" % pattern)
+for row in cursor.fetchall():
+    print "\t".join(row)
