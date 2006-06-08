67ee3899259b351d1999f36a0994e523ce06bfdb
Bob Kline <bkline@rksystems.com>
2006-06-08 22:25:12 +0000

    <R6743> Added code to pack up more doctype info for the client.

--- cdr/Utilities/CheckDtds.py
+++ cdr/Utilities/CheckDtds.py
@@ -1,11 +1,14 @@
 #----------------------------------------------------------------------
 #
-# $Id: CheckDtds.py,v 1.7 2006-01-31 19:10:03 bkline Exp $
+# $Id: CheckDtds.py,v 1.8 2006-06-08 22:25:12 bkline Exp $
 #
 # Utility to reparse the schemas and determine which DTDs are out of
 # date in the manifest for the client.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.7  2006/01/31 19:10:03  bkline
+# Modified to use new client files area.
+#
 # Revision 1.6  2005/08/10 21:13:22  venglisc
 # Minor changes to format the output.
 #
@@ -28,12 +31,40 @@
 
 import cdr, sys
 
-directory = 'd:/cdr/ClientFiles/Rules'
+def getDocTypeResponses(docType):
+    cmd = "<CdrGetDocType Type='%s' OmitDtd='Y' GetEnumValues='Y'/>" % docType
+    response = cdr.sendCommands(cdr.wrapCommand(cmd, 'guest'))
+    start = response.find('<CdrGetDocTypeResp')
+    if not start:
+        sys.stderr.write("CdrGetDocType FAILURE: " + response + "\n")
+        raise Exception("CdrGetDocType FAILURE: " + response)
+    end = response.find('</CdrGetDocTypeResp>')
+    if not end:
+        sys.stderr.write("CdrGetDocType FAILURE: " + response + "\n")
+        raise Exception("CdrGetDocType FAILURE: " + response)
+    return response[start:end] + '</CdrGetDocTypeResp>\n'
+
+def saveDocTypeResponses(docTypeFilePath, docTypeResponses):
+    f = file(docTypeFilePath, 'wb')
+    f.write(docTypeResponses)
+    f.close()
+
+def loadDocTypeResponses(docTypeFilePath):
+    f = file(docTypeFilePath, 'rb')
+    docTypeResponses = f.read()
+    f.close()
+    return docTypeResponses
+
+directory       = '%s/Rules' % cdr.CLIENT_FILES_DIR
+docTypeFileName = 'CdrDocTypes.xml'
+docTypeFilePath = '%s/%s' % (cdr.CLIENT_FILES_DIR, docTypeFileName)
 docTypes = cdr.getDoctypes('guest')
+docTypeResponses = ['<DocTypeResponses>\n']
 for docType in docTypes:
     if docType.upper() in ("FILTER", "CSS"): continue
     try:
         dtInfo = cdr.getDoctype('guest', docType)
+        docTypeResponses.append(getDocTypeResponses(docType))
         #sys.stderr.write("new DTD retrieved\n")
         if not dtInfo.dtd:
             sys.stderr.write("Can't get new DTD for %s\n" % docType)
@@ -60,15 +91,27 @@ for docType in docTypes:
             #sys.stderr.write("old start is at %d\n" % start)
             current = current[start:]
             if newDtd == current: 
-                print "DTD for %22s  is current" % docType
+                print "DTD for %25s  is current" % docType
                 continue
             else: 
-                print "DTD for %22s has changed" % docType
+                print "DTD for %25s has changed" % docType
         else:
-            print "DTD for %22s     added" % docType
+            print "DTD for %25s     added" % docType
         try:
             open(path, "w").write(dtInfo.dtd)
         except:
             sys.stderr.write("failure writing %s\n" % path)
-    except:
-        pass
+    except Exception, e:
+        sys.stderr.write(str(e))
+        #pass
+docTypeResponses.append('</DocTypeResponses>\n')
+docTypeResponses = "".join(docTypeResponses)
+try:
+    if loadDocTypeResponses(docTypeFilePath) != docTypeResponses:
+        print "%s changed" % docTypeFileName
+        saveDocTypeResponses(docTypeFilePath, docTypeResponses)
+    else:
+        print "%s unchanged" % docTypeFileName
+except:
+    print "saving new %s" % docTypeFileName
+    saveDocTypeResponses(docTypeFilePath, docTypeResponses)
