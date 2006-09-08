25f9d36d4b5d97a30bfdcd40616e93ff5bf98f03
ameyer <webcommauto@users.noreply.github.com>
2006-09-08 03:24:22 +0000

    <R6849> Added support for command line host and port. Also checking in the 1 second pause after 50 docs that we added some time ago to workaround a possible problem - which I can't fully remember right now (an sshd bug perhaps?)  We probably don't need this, but it shouldn't do much harm.

--- cdr/Utilities/ReindexByDocType.py
+++ cdr/Utilities/ReindexByDocType.py
@@ -1,16 +1,19 @@
-import cdr, cdrdb, sys
+import cdr, cdrdb, sys, time
 
 if len(sys.argv) < 4:
-    sys.stderr.write("usage: ReindexByDocType uid pwd doctype [max-docs]\n")
+    sys.stderr.write(\
+      "usage: ReindexByDocType uid pwd doctype {max-docs} {host} {port}\n")
     sys.exit(1)
 uid	= sys.argv[1]
 pwd  	= sys.argv[2]
 docType = sys.argv[3]
 maxDocs = len(sys.argv) > 4 and ("TOP %s " % sys.argv[4]) or ""
-session = cdr.login(uid, pwd)
-conn    = cdrdb.connect('CdrGuest')
+host    = len(sys.argv) > 5 and sys.argv[5] or cdr.DEFAULT_HOST
+port    = len(sys.argv) > 6 and int(sys.argv[6]) or cdr.DEFAULT_PORT
+session = cdr.login(uid, pwd, host=host, port=port)
+conn    = cdrdb.connect('CdrGuest', host)
 cursor  = conn.cursor()
-    
+
 cursor.execute("""\
         SELECT %s d.id
           FROM document d
@@ -20,7 +23,13 @@ cursor.execute("""\
       ORDER BY d.id""" % (maxDocs, docType))
 rows = cursor.fetchall()
 print "reindexing %d documents" % len(rows)
+count = 0
 for row in rows:
     print "reindexing CDR%010d" % row[0]
-    resp = cdr.reindex('guest', row[0])
+    resp = cdr.reindex('guest', row[0], host, port)
     if resp: print resp
+
+    # Pause every 50 docs (to avoid swallowing the machine? swamping sshd?)
+    count += 1
+    if count % 50 == 0:
+        time.sleep(1)
