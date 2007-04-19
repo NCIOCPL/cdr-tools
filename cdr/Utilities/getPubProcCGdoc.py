acc0d4c7057bcecb238154f0909d9490c562b760
ameyer <webcommauto@users.noreply.github.com>
2007-04-19 14:32:01 +0000

    <R7312> Initial version.

---
+++ cdr/Utilities/getPubProcCGdoc.py
@@ -0,0 +1,26 @@
+#######################################################
+# Get one XML document from pub_proc_cg to stdout
+#
+# $Id: getPubProcCGdoc.py,v 1.1 2007-04-19 14:32:01 ameyer Exp $
+#
+# $Log: not supported by cvs2svn $
+#
+#######################################################
+import sys, cdrdb, cdr
+
+if len(sys.argv) != 2:
+    sys.stderr.write("usage: getPubProcCGdoc.py docId\n")
+    sys.stderr.write("       Enter docId as plain integer\n")
+    sys.stderr.write("       Writes xml to stdout\n")
+    sys.exit(1)
+
+docId = int(sys.argv[1])
+
+conn = cdrdb.connect("CdrGuest")
+cursor = conn.cursor()
+cursor.execute("SELECT xml FROM pub_proc_cg WHERE id=%d" % docId)
+row = cursor.fetchone()
+if not row:
+    sys.stderr.write("Doc %d not found in pub_proc_cg")
+    sys.exit(1)
+print row[0]
