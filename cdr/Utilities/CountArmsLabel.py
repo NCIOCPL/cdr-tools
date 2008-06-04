d63c1e401d970248158331a77d917f3a2036de13
Volker Englisch <venglisch@users.noreply.github.com>
2008-06-04 18:15:12 +0000

    <R8225> Included the CDR-IDs of the arm trials; sending message to recipients who are member of a group DL.

--- cdr/Utilities/CountArmsLabel.py
+++ cdr/Utilities/CountArmsLabel.py
@@ -1,17 +1,19 @@
 #----------------------------------------------------------------------
 #
-# $Id: CountArmsLabel.py,v 1.1 2008-06-02 14:55:49 venglisc Exp $
+# $Id: CountArmsLabel.py,v 1.2 2008-06-04 18:15:12 venglisc Exp $
 #
 # Count the number of files in the CTGovExport directory that have
 # Arms information (identified by the existence of the arms_grou_label).
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2008/06/02 14:55:49  venglisc
+# Initial copy of script to count the number of protocols submitted to the
+# NLM that have Arms information. The number is submitted to LG.
+#
 #----------------------------------------------------------------------
 import subprocess, os, sys, cdr, time, glob, socket
 
 OUTPUTBASE         = cdr.BASEDIR + "/Output/NLMExport"
-TARNAME            = "clinical_trials.tar.bz2"
-TARCMD             = "d:\\cygwin\\bin\\tar.exe"
 FILEBASE           = "ArmsCount"
 jobTime            = time.localtime(time.time())
 dateStamp          = time.strftime("%Y%m%d%H%M%S", jobTime)
@@ -45,15 +47,33 @@ print 'Result for directory:'
 print '  %s/%s' % (OUTPUTBASE, lastDir)
 print '  %d studies with Arm/Group information' % len(armLines)
 
-machine = socket.gethostname().split('.')[0]
-server  = '%s.nci.nih.gov' % machine
-sender  = '***REMOVED***'
-subject = '%s: Count of NLM ArmsOrGroups Trials' % machine.upper()
-body    = """\nResult for Directory:
+# Setting up email message to be send to users
+# --------------------------------------------
+machine  = socket.gethostname().split('.')[0]
+server   = '%s.nci.nih.gov' % machine
+sender   = '***REMOVED***'
+subject  = '%s: List of NLM Studies with ArmsOrGroups' % machine.upper()
+body     = """\nResult for Directory:
   %s/%s
-  %d studies with Arm/Group information""" % (OUTPUTBASE, lastDir, 
-                                              len(armLines))
-recips = ["***REMOVED***"]
+  %d studies with Arm/Group information
+  
+List of Studies
+===============
+%s""" % (OUTPUTBASE, lastDir, len(armLines), 
+           "".join(id for id in armLines))
+
+# Don't send emails to everyone if we're testing 
+# ----------------------------------------------
+emailDL = cdr.getEmailList('CTGov Export Arms Notification')
+emailDL.sort()
+if machine.upper() == 'BACH':
+    recips = emailDL
+else:
+    if not len(emailDL):
+        recips = ["***REMOVED***"]
+    else:
+        recips = emailDL
+
 if recips:
     cdr.sendMail(sender, recips, subject, body)
 sys.exit()
