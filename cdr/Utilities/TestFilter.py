31f9c34f6a1dd4650bb6586c0cff93e763dc48f8
ameyer <webcommauto@users.noreply.github.com>
2008-12-19 03:49:40 +0000

    <R8681> Added file options for doc and filter.

--- cdr/Utilities/TestFilter.py
+++ cdr/Utilities/TestFilter.py
@@ -3,12 +3,36 @@
 #
 # Run without args for usage info.
 #
-# $Id: TestFilter.py,v 1.1 2008-12-19 03:16:09 ameyer Exp $
+# $Id: TestFilter.py,v 1.2 2008-12-19 03:49:40 ameyer Exp $
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2008/12/19 03:16:09  ameyer
+# Initial version.
+#
 ###########################################################
 
 import sys, getopt, cdr
+
+def getFileContent(fname):
+    """
+    Return the content of a file as a string.
+
+    Pass:
+        filename with optional "file:" at front.
+
+    Return:
+        Contents of file as a string.
+        Exits if error.
+    """
+    try:
+        fp = open(fname, "r")
+        text = fp.read()
+        fp.close()
+    except IOError, info:
+        sys.stderr.write(str(info))
+        sys.exit(1)
+    return text
+
 
 # Options
 fullOutput = True
@@ -17,20 +41,23 @@ for opt in opts:
     if opt[0] == '-p':
         fullOutput = False
 
-
 # usage
 if len(args) < 2:
     sys.stderr.write("""
-usage: TestFilter.py {opts} doc_id filter {"parm=data..." {"parm=data ..."} ...}
+usage: TestFilter.py {opts} Doc Filter {"parm=data..." {"parm=data ..."} ...}
 
  Options:
    -p = Plain output, just the filtered document, no banners or messages.
 
  Arguments:
+   Doc is one of:
+      Doc ID number, no CDR000... prefix.
+      OS file name (if there is one non-digit char).
    Filter is one of:
       Filter doc ID number
       "name:this is a filter name"
       "set:this is a filter set name"
+      "file:OS filter file name"
    Parms are optional name=value pairs, with quotes if required.
    Do not put spaces around '='.
 
@@ -45,16 +72,32 @@ usage: TestFilter.py {opts} doc_id filter {"parm=data..." {"parm=data ..."} ...}
     sys.exit(1)
 
 
-# Doc ID is always an integer
-docId = int(args[0])
+# Doc ID is an integer or a file name
+try:
+    docId = int(args[0])
+except ValueError:
+    docId = None
+    doc = getFileContent(args[0])
+else:
+    doc = None
 
 # Filter can be integer or string, test to find out which
+inline = False
 filter = args[1]
 try:
     filterId = int(filter)
 except ValueError:
-    # String is passed to filterDoc as a sequence of one string
-    filter = [filter,]
+    # Filter specified as a string.
+    # It may be a filename or a "name:" or "set:" CDR identifier
+    if filter.startswith("file:"):
+        filter = getFileContent(filter[5:])
+        inline = True
+    elif filter.startswith("name:") or filter.startswith("set:"):
+        # String is passed to filterDoc as a sequence of one string
+        filter = [filter,]
+    else:
+        sys.stderr.write('Filter identifier "%s" not recognized' % filter)
+        sys.exit(1)
 else:
     # Number passed as integer
     filter = filterId
@@ -68,7 +111,8 @@ while argx < len(args):
 
 # Filter doc
 session = cdr.login("CdrGuest", "never.0n-$undaY")
-resp = cdr.filterDoc(session, filter=filter, docId=docId, parm=parms)
+resp = cdr.filterDoc(session, filter=filter, docId=docId, doc=doc,
+                     inline=inline, parm=parms)
 
 if type(resp) in (type(""), type(u"")):
     sys.stderr.write("Error response:\n  %s" % resp)
