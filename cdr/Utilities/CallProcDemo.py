565ab463ed76c61f21fe61fb33289449a8882c0a
Bob Kline <bkline@rksystems.com>
2009-01-27 19:29:50 +0000

    <R8734> New example program for Alan.

---
+++ cdr/Utilities/CallProcDemo.py
@@ -0,0 +1,28 @@
+#----------------------------------------------------------------------
+#
+# $Id: CallProcDemo.py,v 1.1 2009-01-27 19:29:50 bkline Exp $
+#
+# Illustrates invocation of a stored procedure to obtain the last results
+# set when the number of results sets is unknown.  Assumes that no interim
+# results sets (nor the final results set, for that matter) will be so
+# prohibitively large that keeping the entire set in memory is an unacceptable
+# option.
+#
+# $Log: not supported by cvs2svn $
+#----------------------------------------------------------------------
+import cdrdb
+
+def getLastResultsSet(cursor, procName, parms):
+    cursor.callproc(procName, parms, timeout = 300)
+    lastSet = []
+    done = False
+    while not done:
+        if cursor.description:
+            lastSet = cursor.fetchall()
+        if not cursor.nextset():
+            done = True
+    return lastSet
+
+cursor = cdrdb.connect().cursor()
+parms = []
+print getLastResultsSet(cursor, "select_changed_non_active_protocols", parms)
