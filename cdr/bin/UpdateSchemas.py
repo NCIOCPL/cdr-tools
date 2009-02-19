98e54b353a2c7ed26b14d064259b7008f8f4ef80
Volker Englisch <venglisch@users.noreply.github.com>
2009-02-19 16:16:31 +0000

    <R8789> Added comments and error check for existence of the schema file to display a message when the program isn't doing anything.

--- cdr/bin/UpdateSchemas.py
+++ cdr/bin/UpdateSchemas.py
@@ -1,17 +1,40 @@
+# ************************************************************************
+# Program to replace a schema file with its version in the CDR database.
+# ************************************************************************
 import cdr, sys, glob
+
+# Checking for sufficient command line arguments
+# ----------------------------------------------
 if len(sys.argv) < 4:
-    print "usage: DiffSchemas user-id password schema [schema ...]"
+    print "usage: UpdateSchemas user-id password schema [schema ...]"
     sys.exit(1)
+
+# Creating a session ID to access the CDR database
+# ------------------------------------------------
 session = cdr.login(sys.argv[1], sys.argv[2])
 err = cdr.checkErr(session)
 if err:
     print "failure logging in: " % err
     sys.exit(1)
+
+# Stepping through the command line arguments (i.e. schema files)
+# for processing
+# If the schema doesn't exist in the CDR it will be added, 
+# otherwise the existing schema file will be replaced.
+# --------------------------------------------------------------
 for token in sys.argv[3:]:
-    for schema in glob.glob(token):
+    schemas = glob.glob(token)
+    if not schemas:
+        print "Schema file not in cwd: %s" % token
+        sys.exit(1)
+
+    for schema in schemas:
         print "schema: %s" % schema
         query = "CdrCtl/Title = '%s' and CdrCtl/DocType = 'schema'" % schema
         results = cdr.search(session, query)
+
+        # Schema doesn't exist yet - create new in the CDR
+        # ------------------------------------------------
         if len(results) < 1:
             doc = """\
 <CdrDoc Type='schema' Id=''>
@@ -23,6 +46,8 @@ for token in sys.argv[3:]:
 """ % (schema, open(schema).read())
             id = cdr.addDoc(session, doc = doc, checkIn = 'Y', ver = 'Y')
             print "addDoc: " + id
+        # Schema already exists and needs to be replaced.
+        # -----------------------------------------------
         else:
             for result in results:
                 doc = cdr.getDoc(session, result.docId, 'Y', getObject = 1)
