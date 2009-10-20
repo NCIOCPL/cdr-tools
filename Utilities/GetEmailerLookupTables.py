fc22acf9e9c034f4d1a61a74940e21670160780d
Bob Kline <bkline@rksystems.com>
2009-10-20 12:58:17 +0000

    <R9286> Wrapped script in a try block

--- Utilities/GetEmailerLookupTables.py
+++ Utilities/GetEmailerLookupTables.py
@@ -6,61 +6,68 @@
 # the emailer server.
 #
 #----------------------------------------------------------------------
-import cdr, cdrdb, time, pickle, EmailerProtSites, bz2, cdrmailcommon
 
-start  = time.time()
-tables = []
-conn   = cdrdb.connect('CdrGuest')
-cursor = conn.cursor()
+try:
 
-#----------------------------------------------------------------------
-# Load up the rows for the country lookup table.
-#----------------------------------------------------------------------
-cursor.execute("""\
-    SELECT doc_id, value
-      FROM query_term
-     WHERE path = '/Country/CountryFullName'
-  ORDER BY value""")
-tables.append(('emailer_country', cursor.fetchall()))
+    import cdr, cdrdb, time, pickle, EmailerProtSites, bz2, cdrmailcommon
 
-#----------------------------------------------------------------------
-# Load up the rows for the state lookup table.
-#----------------------------------------------------------------------
-cursor.execute("""\
-    SELECT n.doc_id, n.value, c.int_val
-      FROM query_term n
-      JOIN query_term c
-        ON n.doc_id = c.doc_id
-     WHERE c.path = '/PoliticalSubUnit/Country/@cdr:ref'
-       AND n.path = '/PoliticalSubUnit/PoliticalSubUnitFullName'
-  ORDER BY n.value""")
-tables.append(('emailer_state', cursor.fetchall()))
+    start  = time.time()
+    tables = []
+    conn   = cdrdb.connect('CdrGuest')
+    cursor = conn.cursor()
 
-#----------------------------------------------------------------------
-# Load up the rows for the personal name suffix lookup table.
-#----------------------------------------------------------------------
-dtInfo = cdr.getDoctype('guest', 'Person')
-for vvList in dtInfo.vvLists:
-    if vvList[0] == 'StandardProfessionalSuffix':
-        tables.append(('emailer_suffix', [[col] for col in vvList[1]]))
+    #----------------------------------------------------------------------
+    # Load up the rows for the country lookup table.
+    #----------------------------------------------------------------------
+    cursor.execute("""\
+        SELECT doc_id, value
+          FROM query_term
+         WHERE path = '/Country/CountryFullName'
+      ORDER BY value""")
+    tables.append(('emailer_country', cursor.fetchall()))
 
-#----------------------------------------------------------------------
-# Load up the rows for the protocol site picklist table.
-#----------------------------------------------------------------------
-tables.append(EmailerProtSites.load())
+    #----------------------------------------------------------------------
+    # Load up the rows for the state lookup table.
+    #----------------------------------------------------------------------
+    cursor.execute("""\
+        SELECT n.doc_id, n.value, c.int_val
+          FROM query_term n
+          JOIN query_term c
+            ON n.doc_id = c.doc_id
+         WHERE c.path = '/PoliticalSubUnit/Country/@cdr:ref'
+           AND n.path = '/PoliticalSubUnit/PoliticalSubUnitFullName'
+      ORDER BY n.value""")
+    tables.append(('emailer_state', cursor.fetchall()))
 
-#----------------------------------------------------------------------
-# Upload the new values.
-#----------------------------------------------------------------------
-bytes   = pickle.dumps(tables)
-bytes   = bz2.compress(bytes)
-conn    = cdrmailcommon.emailerConn('dropbox')
-cursor  = conn.cursor()
-cursor.execute("""\
-    INSERT INTO lookup_values (pickle, uploaded)
-         VALUES (%s, NOW())""", bytes)
-conn.commit()
-elapsed = time.time() - start
-print "elapsed: %f" % elapsed
-cdr.logwrite("uploaded emailer lookup values; processing time %f seconds" %
-             elapsed)
+    #----------------------------------------------------------------------
+    # Load up the rows for the personal name suffix lookup table.
+    #----------------------------------------------------------------------
+    dtInfo = cdr.getDoctype('guest', 'Person')
+    for vvList in dtInfo.vvLists:
+        if vvList[0] == 'StandardProfessionalSuffix':
+            tables.append(('emailer_suffix', [[col] for col in vvList[1]]))
+
+    #----------------------------------------------------------------------
+    # Load up the rows for the protocol site picklist table.
+    #----------------------------------------------------------------------
+    tables.append(EmailerProtSites.load())
+
+    #----------------------------------------------------------------------
+    # Upload the new values.
+    #----------------------------------------------------------------------
+    bytes   = pickle.dumps(tables)
+    bytes   = bz2.compress(bytes)
+    conn    = cdrmailcommon.emailerConn('dropbox')
+    cursor  = conn.cursor()
+    cursor.execute("""\
+        INSERT INTO lookup_values (pickle, uploaded)
+             VALUES (%s, NOW())""", bytes)
+    conn.commit()
+    elapsed = time.time() - start
+    print "elapsed: %f" % elapsed
+    cdr.logwrite("uploaded emailer lookup values; processing time %f seconds" %
+                 elapsed)
+
+except Exception, e:
+    import cdr
+    cdr.logwrite(str(e), tback = True)
