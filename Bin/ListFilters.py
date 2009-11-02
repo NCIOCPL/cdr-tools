540ee72aa0b73cb74555abedbcf170cdb2e21e49
Bob Kline <bkline@rksystems.com>
2009-11-02 18:40:37 +0000

    <R9334> Script to list CDR XSL/T filters with their version control filenames.

---
+++ Bin/ListFilters.py
@@ -0,0 +1,21 @@
+#----------------------------------------------------------------------
+#
+# $Id$
+#
+# Command-line script to list filters by name with file names based
+# on Bach CDR IDs.  Useful for finding a CDR filter in a Subversion
+# sandbox.
+#
+#----------------------------------------------------------------------
+import cdrdb, cdr
+
+cursor = cdrdb.connect('CdrGuest', dataSource=cdr.PROD_HOST).cursor()
+cursor.execute("""\
+    SELECT d.id, d.title
+      FROM document d
+      JOIN doc_type t
+        ON t.id = d.doc_type
+     WHERE t.name = 'filter'
+  ORDER BY d.title""")
+for docId, docTitle in cursor.fetchall():
+    print "CDR%010d.xml %s" % (docId, docTitle.encode('utf-8'))
