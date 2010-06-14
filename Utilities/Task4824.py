ea8089163ee93cbf51f1ea0911a5a98be0d33fd1
Bob Kline <bkline@rksystems.com>
2010-06-14 15:43:09 +0000

    <R9694> Added column for NCT ID at William's request (BZIssue::4824)

--- Utilities/Task4824.py
+++ Utilities/Task4824.py
@@ -2,33 +2,42 @@
 #
 # $Id$
 #
+# Find trials with "DUP" as substring in selected comment fields.
+#
+# BZIssue::4824
+#
 #----------------------------------------------------------------------
 import lxml.etree, cdrdb, cgi, sys
 
+def fix(me):
+    if not me:
+        return ''
+    return cgi.escape(me).encode('utf-8')
+
 cursor = cdrdb.connect('CdrGuest', dataSource='bach.nci.nih.gov').cursor()
 cursor.execute("""\
-    SELECT DISTINCT n.doc_id
+    SELECT DISTINCT n.doc_id, n.value
                FROM query_term n
                JOIN query_term t
                  ON t.doc_id = n.doc_id
                 AND LEFT(t.node_loc, 4) = LEFT(n.node_loc, 4)
-               JOIN pub_proc_cg c
-                 ON c.id = n.doc_id
               WHERE t.value = 'ClinicalTrials.gov ID'
                 AND t.path = '/InScopeProtocol/ProtocolIDs/OtherID/IDType'
                 AND n.path = '/InScopeProtocol/ProtocolIDs/OtherID/IDString'
                 AND n.value LIKE 'NCT%'
            ORDER BY 1""", timeout=300)
-docIds = [row[0] for row in cursor.fetchall()]
+rows = cursor.fetchall()
 print """\
 <html>
  <table style='font-family: Arial;' border='1' cellpadding='2' cellspacing='0'>
   <tr style='color: green'>
    <th>CDR ID</th>
+   <th>NLM ID</th>
    <th>Duplicate Comment</th>
   </tr>"""
-for i, docId in enumerate(docIds):
-    cursor.execute("SELECT xml FROM document WHERE id = ?", docId)
+for i, row in enumerate(rows):
+    cdrId, nlmId = row
+    cursor.execute("SELECT xml FROM document WHERE id = ?", cdrId)
     try:
         docXml = cursor.fetchall()[0][0]
         tree = lxml.etree.XML(docXml.encode('utf-8'))
@@ -39,18 +48,21 @@ for i, docId in enumerate(docIds):
             comments.append(node.text)
         for comment in comments:
             if 'DUP' in comment.upper():
-                print (u"""\
+                print u"""\
   <tr>
    <td>%d</td>
    <td>%s</td>
-  </tr>""" % (docId, cgi.escape(comment))).encode('utf-8')
+   <td>%s</td>
+  </tr>""" % (cdrId, fix(nlmId), fix(comment))
     except Exception, e:
         print (u"""\
   <tr style='color: red'>
    <td>%d</td>
    <td>%s</td>
-  </tr>""" % (docId, u"FAILURE: %s" % cgi.escape(unicode(e)))).encode('utf-8')
-    sys.stderr.write("\rprocessed %d of %d documents" % (i + 1, len(docIds)))
+   <td>%s</td>
+  </tr>""" % (cdrId, nlmId,
+              u"FAILURE: %s" % cgi.escape(unicode(e)))).encode('utf-8')
+    sys.stderr.write("\rprocessed %d of %d documents" % (i + 1, len(rows)))
 print """\
  </table>
 </html>"""
