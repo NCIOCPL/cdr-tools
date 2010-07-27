0115b331e5b7903e5e28bf90ecfecde429396b37
Bob Kline <bkline@rksystems.com>
2010-07-27 13:00:20 +0000

    <R9809> Modifications requested by Lakshmi (BZIssue::4878)

--- Utilities/Request4878.py
+++ Utilities/Request4878.py
@@ -23,15 +23,7 @@
 # BZIssue::4878
 #
 #----------------------------------------------------------------------
-import cdrdb, sys, csv, lxml.etree as etree
-
-def fix(me):
-    if not me:
-        return ''
-    return me.strip().encode('utf-8')
-
-def fixList(l):
-    return "; ".join([fix(m) for m in l])
+import cdrdb, sys, ExcelReader, ExcelWriter, lxml.etree as etree
 
 class Trial:
     def __init__(self, cdrId, cursor):
@@ -49,16 +41,23 @@ class Trial:
         self.docType = tree.tag
         if self.docType == 'InScopeProtocol':
             for node in tree.findall('ProtocolTitle[@Type="Professional"]'):
-                self.title = node.text
+                self.title = node.text.strip()
             for node in tree.findall('ProtocolIDs'):
                 for child in node.findall('PrimaryID/IDString'):
-                    self.primaryId = child.text
+                    self.primaryId = child.text.strip()
                 for child in node.findall('OtherID/IDString'):
-                    self.otherIds.append(child.text)
-            for node in tree.findall('ProtocolDetail/StudyCategoryName'):
-                self.trialTypes.append(node.text)
+                    self.otherIds.append(child.text.strip())
+            for node in tree.findall('ProtocolDetail/StudyCategory'):
+                categoryName = categoryType = None
+                for child in node:
+                    if child.tag == 'StudyCategoryType':
+                        categoryType = child.text
+                    elif child.tag == 'StudyCategoryName':
+                        categoryName = child.text
+                if categoryName and categoryType =='Primary':
+                    self.trialTypes.append(categoryName.strip())
             for node in tree.findall('ProtocolPhase'):
-                self.phases.append(node.text)
+                self.phases.append(node.text.strip())
             for node in tree.findall('ProtocolAdminInfo/ProtocolLeadOrg'):
                 for child in node.findall('LeadOrgPersonnel'):
                     for role in child.findall('PersonRole'):
@@ -66,38 +65,99 @@ class Trial:
                                                  'PROTOCOL CO-CHAIR',
                                                  'PRINCIPAL INVESTIGATOR'):
                             for person in child.findall('Person'):
-                                self.piNames.append(person.text.split('[')[0])
+                                name = person.text.split('[')[0].strip()
+                                self.piNames.append(name)
             for node in tree.findall('ExpectedEnrollment'):
                 self.expectedEnrollment = node.text
         else:
             for node in tree.findall('OfficialTitle'):
-                self.title = node.text
+                self.title = node.text.strip()
             for node in tree.findall('IDInfo'):
                 for child in node.findall('OrgStudyID'):
-                    self.primaryId = child.text
+                    self.primaryId = child.text.strip()
                 for child in node.findall('NCTID'):
-                    self.otherIds.append(child.text)
+                    self.otherIds.append(child.text.strip())
                 for child in node.findall('SecondaryID'):
-                    self.otherIds.append(child.text)
-            for node in tree.findall('PDQIndexing/StudyCategory'
-                                     '/StudyCategoryName'):
-                self.trialTypes.append(node.text)
+                    self.otherIds.append(child.text.strip())
+            for node in tree.findall('PDQIndexing/StudyCategory'):
+                categoryName = categoryType = None
+                for child in node:
+                    if child.tag == 'StudyCategoryType':
+                        categoryType = child.text
+                    elif child.tag == 'StudyCategoryName':
+                        categoryName = child.text
+                if categoryName and categoryType =='Primary':
+                    self.trialTypes.append(categoryName)
             for node in tree.findall('Phase'):
-                self.phases.append(node.text)
+                self.phases.append(node.text.strip())
             for node in tree.findall('Sponsors/OverallOfficial/Surname'):
-                self.piNames.append(node.text)
+                self.piNames.append(node.text.strip())
             for node in tree.findall('CTGovIndexing/CTExpectedEnrollment'):
-                self.expectedEnrollment = node.text
-cursor = cdrdb.connect('CdrGuest').cursor()
-fp = open('Request4878.txt', 'w')
-writer = csv.writer(fp)
-writer.writerow(('CDRID', 'Title', 'Primary ID', 'Additional IDs',
-                 'Type of Trial', 'Phase', 'PI Name', 'Expected Enrollment'))
+                self.expectedEnrollment = node.text.strip()
+def main():
 
-for cdrId in (564231, 579635, 582085, 583574, 586896, 658956, 538204, 566023):
-    trial = Trial(cdrId, cursor)
-    writer.writerow((trial.cdrId, fix(trial.title), fix(trial.primaryId),
-                     fixList(trial.otherIds), fixList(trial.trialTypes),
-                     fixList(trial.phases), fixList(trial.piNames),
-                     fix(trial.expectedEnrollment)))
-fp.close()
+    # Collect the document IDs for the trials to be reported.
+    book = ExcelReader.Workbook(sys.argv[1])
+    sheet = book[0]
+    docIds = [int(row[0].val) for row in sheet]
+
+    # Create the spreadsheet (cloned from OSP report in CdrLongReports.py).
+    title = "Clinical Trials for Pancreatic Cancer"
+    wb = ExcelWriter.Workbook()
+    b = ExcelWriter.Border()
+    borders = ExcelWriter.Borders(b, b, b, b)
+    font = ExcelWriter.Font(name = 'Times New Roman', size = 10)
+    align = ExcelWriter.Alignment('Left', 'Top', wrap = True)
+    style1 = wb.addStyle(alignment = align, font = font, borders = borders)
+    urlFont = ExcelWriter.Font('blue', None, 'Times New Roman', size = 10)
+    style2 = wb.addStyle(alignment = align, font = urlFont, borders = borders)
+    ws = wb.addWorksheet("PDQ Clinical Trials", style1, 40, 2)
+    
+    ws.addCol(1, 50)
+    ws.addCol(2, 250)
+    ws.addCol(3, 100)
+    ws.addCol(4, 125)
+    ws.addCol(5, 85)
+    ws.addCol(6, 85)
+    ws.addCol(7, 100)
+    ws.addCol(8, 100)
+
+    # Set up the title and header cells in the spreadsheet's top rows.
+    font = ExcelWriter.Font(name = 'Times New Roman', bold = True, size = 10)
+    align = ExcelWriter.Alignment('Center', 'Center', wrap = True)
+    interior = ExcelWriter.Interior('#CCFFCC')
+    style3 = wb.addStyle(alignment = align, font = font, borders = borders,
+                         interior = interior)
+    headings = ('CDRID', 'Title', 'Primary ID', 'Additional IDs',
+                 'Type of Trial', 'Phase', 'PI Name', 'Expected Enrollment')
+    row = ws.addRow(1, style3, 40)
+    row.addCell(1, title, mergeAcross = len(headings) - 1)
+    row = ws.addRow(2, style3, 40)
+    for i in range(len(headings)):
+        row.addCell(i + 1, headings[i])
+
+    cursor = cdrdb.connect('CdrGuest').cursor()
+    rowNum = 3
+    for docId in docIds:
+        trial = Trial(docId, cursor)
+        row = ws.addRow(rowNum, style1, 40)
+        tip = ("Left-click cell with mouse to view the protocol document.  "
+               "Left-click and hold to select the cell.")
+        url = ("http://www.cancer.gov/clinicaltrials/"
+               "view_clinicaltrials.aspx?version=healthprofessional&"
+               "cdrid=%d" % trial.cdrId)
+        row.addCell(1, trial.cdrId)
+        row.addCell(2, trial.title)
+        row.addCell(3, trial.primaryId, href = url, tooltip = tip,
+                    style = style2)
+        row.addCell(4, u"; ".join(trial.otherIds))
+        row.addCell(5, u"; ".join(trial.trialTypes))
+        row.addCell(6, u"; ".join(trial.phases))
+        row.addCell(7, u"; ".join(trial.piNames) or u"NOT SPECIFIED")
+        row.addCell(8, trial.expectedEnrollment)
+        rowNum += 1
+    fp = open('d:/tmp/Request4878.xls', 'wb')
+    wb.write(fp, True)
+    fp.close()
+if __name__ == '__main__':
+    main()
