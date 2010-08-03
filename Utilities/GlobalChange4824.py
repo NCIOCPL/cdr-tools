d557101ac6f4c2d16b7bfa71e3efc5cc7669669a
Bob Kline <bkline@rksystems.com>
2010-08-03 14:19:05 +0000

    <R9823> Implemented what the users decided they wanted (insertion of empty CTGovDuplicate elements) (BZIssue::4824)

--- Utilities/GlobalChange4824.py
+++ Utilities/GlobalChange4824.py
@@ -7,7 +7,7 @@
 # BZIssue::4824
 #
 #----------------------------------------------------------------------
-import ExcelReader, cdrdb, ModifyDocs, sys, lxml.etree as etree
+import cdr, ExcelReader, ModifyDocs, sys, lxml.etree as etree
 
 LOGFILE = 'd:/cdr/log/GlobalChange4824.log'
 
@@ -18,39 +18,40 @@ LOGFILE = 'd:/cdr/log/GlobalChange4824.log'
 # object's xml member.
 #----------------------------------------------------------------------
 class GlobalChange4824:
-    class Trial:
-        def __init__(self, row, cursor):
-            self.cdrId = int(row[0].val]
-            self.nctId = row[1].val
-            cursor.execute("""\
-                SELECT ...""")
-    def __init__(self, workbookName, cursor):
-        book = ExcelReader(workbookName)
+    def __init__(self, workbookName):
+        book = ExcelReader.Workbook(workbookName)
         sheet = book[0]
-        self.trials = {}
+        self.docIds = []
         for row in sheet:
             try:
-                trial = GlobalChange4824.Trial(row, cursor)
-                self.trials[trial.cdrId] = trial
+                self.docIds.append(int(row[0].val))
             except:
                 pass
     def getDocIds(self):
-        return self.trials.keys()
+        return self.docIds
     def run(self, docObj):
+        tree = etree.XML(docObj.xml)
+        position = 0
+        for node in tree:
+            if node.tag == 'CTGovDuplicate':
+                break
+            elif node.tag == 'ProtocolIDs':
+                tree.insert(position, etree.Element('CTGovDuplicate'))
+                break
+            else:
+                position += 1
+        return etree.tostring(tree, encoding='utf-8', xml_declaration=True)
         
 #----------------------------------------------------------------------
 # Collect the data to be added to the protocol docs.
 #----------------------------------------------------------------------
 if len(sys.argv) < 5 or sys.argv[4] not in ('test', 'live'):
-    sys.stderr.write("usage: Request4835.py uid pwd input-doc test|live\n")
+    sys.stderr.write("usage: Request4824.py uid pwd input-doc test|live\n")
     sys.exit(1)
-fileName = sys.argv[3]
-xmlDoc   = open(fileName, 'rb').read()
-tree     = etree.XML(xmlDoc)
-obj      = FilterAndTransform(tree)
+obj = GlobalChange4824(sys.argv[3])
 testMode = sys.argv[4] == 'test'
 cdr.logwrite("running in %s mode" % sys.argv[4], LOGFILE)
 job = ModifyDocs.Job(sys.argv[1], sys.argv[2], obj, obj,
                      "Adding CTGovDuplicate elements (request #4824).",
-                     testMode = testMode, logFile = LOGFILE)
+                     testMode=testMode, logFile=LOGFILE)
 job.run()
