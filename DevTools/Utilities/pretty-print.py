006d7f7ca1dc7444bca796c5e8e129239f10a93d
Bob Kline <bkline@rksystems.com>
2016-04-28 15:20:21 +0000

    <R13983> filter to make XML document easier to edit

---
+++ DevTools/Utilities/pretty-print.py
@@ -0,0 +1,10 @@
+import lxml.etree as etree
+import sys
+
+if len(sys.argv) < 2:
+    source = sys.stdin
+else:
+    source = sys.argv[1]
+parser = etree.XMLParser(remove_blank_text=True)
+tree = etree.parse(source, parser)
+print etree.tostring(tree, encoding="utf-8", pretty_print=True)
