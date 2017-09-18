19c09277866fa94f755b0cd21e55e0c2d2cc90e2
Bob Kline <bkline@rksystems.com>
2017-09-18 15:05:13 -0400

    Rewrite build scripts for GitHub/Jenkins (OCECDR-4300)

--- Build/build-cdr-bin.cmd
+++ Build/build-cdr-bin.cmd
@@ -1,12 +1,12 @@
 @REM ----------------------------------------------------------------------
-@REM Build the contents of the /cdr/bin directory for deployment.
+@REM Build the contents of the /cdr/Bin directory for deployment.
 @REM ----------------------------------------------------------------------
 
-@REM ECHO OFF
+@ECHO OFF
 SETLOCAL
 SET SCRIPTNAME=%0
 CALL :init %*           || EXIT /B 1
-CALL :pull_svn_files    || EXIT /B 1
+CALL :pull_github_files || EXIT /B 1
 CALL :pull_msvc_dlls    || EXIT /B 1
 CALL :build_server      || EXIT /B 1
 CALL :build_service     || EXIT /B 1
@@ -15,124 +15,73 @@ CALL :cleanup           || EXIT /B 1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Create work spaces and set environment variables.
+REM Verify options and set local variables.
 REM ----------------------------------------------------------------------
 :init
-ECHO Building CDR Bin directory.
-IF "%1." == "." (
-    ECHO Usage: CALL %SCRIPTNAME% branch-path [svn-pwd [svn-uid]]
-    ECHO  e.g.: CALL %SCRIPTNAME% branches/patch-2.3
+IF "%3." == "." (
+    ECHO Usage: CALL %SCRIPTNAME% BRANCH OUTPUT-BASE CDR-DRIVE
+    ECHO  e.g.: CALL %SCRIPTNAME% fermi d:\tmp\fermi D
     EXiT /B 1
 )
-
-REM Establish defaults for all CDRBUILD_ environment variables
-CALL init-build-envvars.cmd
-IF "%2." == "." (
-    SET SVNEXP=%CYGSVN% export %CDRBUILD_SVNOPTS%
-) ELSE IF "%3." == "." (
-    SET SVNEXP=%CYGSVN% export %CDRBUILD_SVNOPTS% --password %2
-) ELSE (
-    SET SVNEXP=%CYGSVN% export %CDRBUILD_SVNOPTS% --username %3 --password %2
-)
-
-REM Establish important environment variables
-CALL init-build-envvars.cmd
-
-REM Output data to a directory name found in the env (created just above)
-SET BUILDDIR_TMP=%CDRBUILD_BASEPATH%
-SET BUILDDIR=%BUILDDIR_TMP:/=\%
-SET BINDIR=%BUILDDIR%\Bin
-ECHO Will copy all files into %BINDIR%
-
-SET SVNBRANCH=%CDRBUILD_SVNBASEURL%/%1
-SET CYGDATE=%CDRBUILD_CYGBIN%\date.exe
-SET STAMP=
-FOR /F %%s IN ('%CYGDATE% +%%Y%%m%%d%%H%%M%%S') DO SET STAMP=%%s
-IF NOT DEFINED STAMP ECHO %CYGDATE% failure && EXIT /B 1
-
-SET WORKDIR=%CDRBUILD_DRIVE%\tmp\cdr-bin-%STAMP%
-MKDIR %WORKDIR% || ECHO Failure creating %WORKDIR% && EXIT /B 1
-
-CD %WORKDIR%
-ECHO Created working directory.
-CALL %CDRBUILD_DRIVE%\bin\vcvars32.bat > NUL 2>&1 || ECHO Failed VC Init && EXIT /B 1
-ECHO Compiler successfully initialized.
-MKDIR %BUILDDIR% >NUL 2>&1
-RMDIR /S /Q %BINDIR% >NUL 2>&1
-ECHO Environment successfully initialized.
+SET BRANCH=%1
+SET TARGET=%2
+SET DRIVE=%3
+SET NCIOCPL=https://github.com/NCIOCPL
+%DRIVE%:\bin\vsvars32.bat || ECHO Failed Visual Studio Init && EXIT /B 1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Get the files which can be pulled directly from version control.
-REM This creates the output directory for the AHP artifact, so it
-REM must be done early.
+REM Pull what we need from GitHub.
 REM ----------------------------------------------------------------------
-:pull_svn_files
-ECHO Exporting scripts from Subversion.
-CD %BUILDDIR%
-cd
-%SVNEXP% %SVNBRANCH%/Bin || ECHO Failed export && EXIT /B 1
-ECHO Scripts successfully exported from Subversion.
+:pull_github_files
+CD /D %TARGET%
+SET URL=%NCIOCPL%/cdr-tools/branches/%BRANCH%/Bin
+svn export -q %URL% || ECHO Failure exporting %URL% && EXIT /B 1
+CD Bin
+SET URL=%NCIOCPL%/cdr-server/branches/%BRANCH%/Server
+svn export -q %URL% tmp-build || ECHO Failure exporting %URL% && EXIT /B 1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Get the Microsoft runtime DLLs.
+REM Grab a copy of Microsoft's runtime DLLs.
 REM ----------------------------------------------------------------------
 :pull_msvc_dlls
-ECHO Preserving Microsoft runtime DLLs.
-ECHO %BINDIR%
-SET BINDIR
-ECHO COPY %CDRBUILD_DRIVE%\cdr\Bin\msvc*.dll %BINDIR%
-COPY %CDRBUILD_DRIVE%\cdr\Bin\msvc*.dll %BINDIR% >NUL 2>&1 || ECHO Copy Failed && EXIT /B 1
-ECHO Microsoft runtime DLLs successfully fetched.
+SET DLLS=%DRIVE%:\cdr\Bin\msvc*.dll
+COPY %DLLS% %TARGET%\Bin || ECHO Unable to copy MSVC DLLs && EXIT /B 1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Build the CDR server executable.
+REM Build the CDR Server.
 REM ----------------------------------------------------------------------
 :build_server
-ECHO Building CDR Server
-CD %WORKDIR%
-%SVNEXP% %SVNBRANCH%/Server || ECHO Failed export && EXIT /B 1
-CD Server
-
-nmake DRV=%CDRBUILD_DRIVE% CdrServer.exe >>log 2>>err || ECHO Server Build Failed && EXIT /B 1
-COPY CdrServer.exe %BINDIR%\ > NUL 2>&1
-ECHO Server built successfully.
+CD /D %TARGET%\Bin\tmp-build
+nmake DRV=%DRIVE%: CdrServer.exe >>log 2>>err || ECHO Build failed && EXIT /B 1
+COPY CdrServer.exe .. > NUL 2>&1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Build the CDR service executable.  We already have the source.
+REM Build the CDR Windows Service executable.
 REM ----------------------------------------------------------------------
 :build_service
-ECHO Building CDR Windows Service executable.
-CD %WORKDIR%\Server
-nmake CdrService.exe >>log 2>>err || ECHO Service Build Failed && EXIT /B 1
-COPY CdrService.exe %BINDIR% > NUL 2>&1
-ECHO Service built successfully.
+CD /D %TARGET%\Bin\tmp-build
+nmake CdrService.exe >>log 2>>err || ECHO Building service failed && EXIT /B 1
+COPY CdrService.exe .. > NUL 2>&1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Build the CDR Server shutdown program.  We already have the source.
+REM Build the CDR Server shutdown program.
 REM ----------------------------------------------------------------------
 :build_shutdown
-ECHO Building CDR Server shutdown program.
-CD %WORKDIR%\Server
-nmake ShutdownCdr.exe >>log 2>>err || ECHO Shutdown Build Failed && EXIT /B 1
-COPY ShutdownCdr.exe %BINDIR% > NUL 2>&1
-ECHO Shutdown program built successfully.
+CD /D %TARGET%\Bin\tmp-build
+nmake ShutdownCdr.exe >>log 2>>err || ECHO Build Shutdown failure && EXIT /B 1
+COPY ShutdownCdr.exe .. > NUL 2>&1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Set file permissions and drop our working intermediate files.
+REM Cleanup temporary files.
 REM ----------------------------------------------------------------------
 :cleanup
-ECHO Setting file permssions.
-CD %BINDIR%
-%CDRBUILD_CYGBIN%\chmod -R 777 * || ECHO Can't set permissions && EXIT /B 1
-ECHO File permissions successfully set.
-ECHO Cleaning up temporary files.
-CD \
-RMDIR /S /Q %WORKDIR% || ECHO Cleanup failure && EXIT /B 1
+CD /D %TARGET%\Bin
+RMDIR /S /Q tmp-build || ECHO Failure cleaning up build && EXIT /B 1
 ECHO Build complete.
 EXIT /B 0
--- Build/build-client-files.cmd
+++ Build/build-client-files.cmd
@@ -7,164 +7,100 @@ SETLOCAL
 SET SCRIPTNAME=%0
 @REM Invoke each subscript, exit /B 1 inside any of them causes abort
 CALL :init %*           || EXIT /B 1
-CALL :pull_svn_files    || EXIT /B 1
+CALL :pull_github_files || EXIT /B 1
 CALL :build_loader      || EXIT /B 1
 CALL :build_dll         || EXIT /B 1
 CALL :build_dtds        || EXIT /B 1
 CALL :build_manifest    || EXIT /B 1
-CALL :export_schemas    || EXIT /B 1
 CALL :cleanup           || EXIT /B 1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Create work spaces and set environment variables.
+REM Verify options, set local variables, and create the directory.
 REM ----------------------------------------------------------------------
 :init
-ECHO Building CDR Client Files.
-IF "%1." == "." (
-    ECHO Usage: CALL %SCRIPTNAME% branch-path [svn-pwd [svn-uid]]
-    ECHO  e.g.: CALL %SCRIPTNAME% branches/patch-2.3
-    EXIT /B 1
+IF "%4." == "." (
+    ECHO Usage: CALL %SCRIPTNAME% BRANCH OUTPUT-BASE CDR-DRIVE TIMESTAMP
+    ECHO  e.g.: CALL %SCRIPTNAME% fermi d:\tmp\fermi D 20170918132844
+    EXiT /B 1
 )
-
-REM Establish defaults for all CDRBUILD_ environment variables
-CALL init-build-envvars.cmd
-
-REM Create command to export from svn
-IF "%2." == "." (
-    SET SVNEXP=%CYGSVN% export %CDRBUILD_SVNOPTS%
-) ELSE IF "%3." == "." (
-    SET SVNEXP=%CYGSVN% export %CDRBUILD_SVNOPTS% --password %2
-) ELSE (
-    SET SVNEXP=%CYGSVN% export %CDRBUILD_SVNOPTS% --username %3 --password %2
-)
-
-REM Output data to a directory name found in the env or created here
-IF "%CDRBUILD_BASEPATH%." == "." (
-  SET CLIENTFILES_TMP=%CDRBUILD_DRIVE%\tmp\ClientFiles
-) ELSE (
-  SET CLIENTFILES_TMP=%CDRBUILD_BASEPATH%\ClientFiles
-)
-SET CLIENTFILES=%CLIENTFILES_TMP:/=\%
-
-SET SVNBRANCH=%CDRBUILD_SVNBASEURL%/%1
-SET CYGDATE=%CDRBUILD_CYGBIN%\date.exe
-SET STAMP=
-FOR /F %%s IN ('%CYGDATE% +%%Y%%m%%d%%H%%M%%S') DO SET STAMP=%%s
-IF NOT DEFINED STAMP ECHO %CYGDATE% failure && EXIT /B 1
-SET WORKDIR=%CDRBUILD_DRIVE%\tmp\client-%STAMP%
-SET CDRLOADER=CdrClient-%STAMP%.exe
-MKDIR %WORKDIR% || ECHO Failure creating %WORKDIR% && EXIT /B 1
-CD %WORKDIR%
-ECHO Created working directory.
-%SVNEXP% %SVNBRANCH%/Build/AnthillPro || ECHO Failed export && EXIT /B 1
-ECHO Build tools successfully fetched into working directory.
-CALL %CDRBUILD_DRIVE%\bin\vcvars32.bat > NUL 2>&1 || ECHO Failed Visual C++ Init (vcvars32) && EXIT /B 1
-ECHO Compiler successfully initialized.
-IF EXIST %CLIENTFILES% (
-  RMDIR /S /Q %CLIENTFILES%
-)
+SET BRANCH=%1
+SET TARGET=%2
+SET DRIVE=%3
+SET STAMP=%4
+SET NCIOCPL=https://github.com/NCIOCPL
+SET CLIENTFILES=%TARGET%\ClientFiles
+SET LOADER=CdrClient-%STAMP%.exe
 MKDIR %CLIENTFILES% || ECHO Failed creating %CLIENTFILES% && EXIT /B 1
-ECHO Environment successfully initialized.
+%DRIVE%:\bin\vsvars32.bat || ECHO Failed Visual Studio Init && EXIT /B 1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Get the files which can be pulled directly from version control.
+REM Pull what we need from GitHub.
 REM ----------------------------------------------------------------------
-:pull_svn_files
-ECHO Exporting files from Subversion.
-CD %CLIENTFILES%
-%SVNEXP% %SVNBRANCH%/XMetaL/Display   || ECHO Failed export && EXIT /B 1
-%SVNEXP% %SVNBRANCH%/XMetaL/Forms     || ECHO Failed export && EXIT /B 1
-%SVNEXP% %SVNBRANCH%/XMetaL/Icons     || ECHO Failed export && EXIT /B 1
-%SVNEXP% %SVNBRANCH%/XMetaL/Macros    || ECHO Failed export && EXIT /B 1
-%SVNEXP% %SVNBRANCH%/XMetaL/Rules     || ECHO Failed export && EXIT /B 1
-%SVNEXP% %SVNBRANCH%/XMetaL/Template  || ECHO Failed export && EXIT /B 1
-ECHO Client configuration files pulled successfully from Subversion.
+:pull_github_files
+CHDIR /D %CLIENTFILES%
+SET URL=%NCIOCPL%/cdr-client/branches/%BRANCH%/XMetaL
+SET BUILD=%NCIOCPL%/cdr-tools/branches/%BRANCH%/Build
+SET EXP=svn export -q
+%EXP% %URL%/Display   || ECHO Failure exporting Display   && EXIT /B 1
+%EXP% %URL%/Forms     || ECHO Failure exporting Forms     && EXIT /B 1
+%EXP% %URL%/Icons     || ECHO Failure exporting Icons     && EXIT /B 1
+%EXP% %URL%/Macros    || ECHO Failure exporting Macros    && EXIT /B 1
+%EXP% %URL%/Rules     || ECHO Failure exporting Rules     && EXIT /B 1
+%EXP% %URL%/Template  || ECHO Failure exporting Template  && EXIT /B 1
+MKDIR tmp-build       || ECHO Failure creating tmp-build  && EXIT /B 1
+CHDIR tmp-build       || ECHO Failure entering tmp-build  && EXIT /B 1
+%EXP% %URL%/CdrClient || ECHO Failure exporting CdrClient && EXIT /B 1
+%EXP% %URL%/DLL       || ECHO Failure exporting DLL       && EXIT /B 1
+%EXP% %BUILD%         || ECHO Failure exporting Build     && EXIT /B 1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
 REM Build the program which launches XMetaL.
 REM ----------------------------------------------------------------------
 :build_loader
-ECHO Building Client CDR Loader.
-CD %WORKDIR%
-%SVNEXP% %SVNBRANCH%/XMetaL/CdrClient || ECHO Failed export && EXIT /B 1
-CD CdrClient
+CHDIR /D %CLIENTFILES%\tmp-build\CdrClient
 nmake > nmake.log 2>nmake.err || ECHO Failed building loader && EXIT /B 1
-COPY Release\CdrClient.exe %CLIENTFILES%\%CDRLOADER% > NUL 2>&1
-CD %CLIENTFILES%
-python %WORKDIR%\AnthillPro\make-cdr-loader-scripts.py %CDRLOADER%
-ECHO Loader successfully built.
+COPY Release\CdrClient.exe %CLIENTFILES%\%LOADER% > NUL 2>&1
+CHDIR /D %CLIENTFILES%
+SET SCRIPT=tmp-build\Build\make-cdr-loader-scripts.py
+python %SCRIPT% %LOADER% || ECHO Failed building loader scripts && EXIT /B 1
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
 REM Build the DLL used by the XMetaL client.
 REM ----------------------------------------------------------------------
 :build_dll
-ECHO Building Client DLL.
-CD %WORKDIR%
-%SVNEXP% %SVNBRANCH%/XMetaL/DLL || ECHO Failed export && EXIT /B 1
-CD DLL
-nmake > nmake.log 2>nmake.err || ECHO DLL build failure, check nmake.log && EXIT /B 1
+CHDIR /D %CLIENTFILES%\tmp-build\DLL
+nmake > nmake.log 2>nmake.err || ECHO DLL build failure && EXIT /B 1
 MKDIR %CLIENTFILES%\Cdr
 COPY ReleaseUMinDependency\Cdr.dll %CLIENTFILES%\Cdr\Cdr.dll > NUL 2>&1
-ECHO DLL successfully built.
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
 REM Generate the DTDs from the repository's schemas.
 REM ----------------------------------------------------------------------
 :build_dtds
-ECHO Building DTDs.
-CD %WORKDIR%\AnthillPro
+CHDIR /D %CLIENTFILES%\tmp-build\Build
 python CheckDtds.py %CLIENTFILES% >CheckDtds.log 2>CheckDtds.err
 IF ERRORLEVEL 1 ECHO Failure generating DTDs && EXIT /B 1
-ECHO DTDs successfully generated.
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
 REM Generate the manifest for the client files.
 REM ----------------------------------------------------------------------
 :build_manifest
-ECHO Building fresh manifest file.
-CD %WORKDIR%\AnthillPro
+CHDIR /D %CLIENTFILES%\tmp-build\Build
 python RefreshManifest.py %CLIENTFILES% >RefreshManifest.err 2>&1
 IF ERRORLEVEL 1 ECHO Failure building manifest && EXIT /B 1
-ECHO Manifest successfully built.
 EXIT /B 0
 
 REM ----------------------------------------------------------------------
-REM Export the current schemas for later deployment
-REM ----------------------------------------------------------------------
-:export_schemas
-ECHO Exporting current schemas from version control
-
-REM Output path
-IF "%CDRBUILD_BASEPATH%." == "." (
-  SET SCHEMAFILES_TMP=%CDRBUILD_DRIVE%\tmp
-) ELSE (
-  SET SCHEMAFILES_TMP=%CDRBUILD_BASEPATH%
-)
-SET SCHEMAFILES=%SCHEMAFILES_TMP:/=\%
-CD %SCHEMAFILES%
-%SVNEXP% %SVNBRANCH%/Schemas || ECHO Failed Schemas export && EXIT /B 1
-ECHO Schemas exported successfully.
-EXIT /B 0
-
-REM ----------------------------------------------------------------------
-REM Set file permissions and drop our working intermediate files.
+REM Drop our working intermediate files.
 REM ----------------------------------------------------------------------
 :cleanup
-ECHO Setting file permssions.
-CD %CLIENTFILES%
-echo DEBUG
-echo CLIENTFILES=%CLIENTFILES%
-%CDRBUILD_CYGBIN%\pwd
-%CDRBUILD_CYGBIN%\chmod -R 777 * || ECHO Can't set permissions && EXIT /B 1
-ECHO File permissions successfully set.
-ECHO Cleaning up temporary files.
-CD \
-RMDIR /S /Q %WORKDIR% || ECHO Cleanup failure && EXIT /B 1
+CHDIR %CLIENTFILES%
+RMDIR /S /Q tmp-build || ECHO Cleanup failure && EXIT /B 1
 ECHO Build complete.
 EXIT /B 0
