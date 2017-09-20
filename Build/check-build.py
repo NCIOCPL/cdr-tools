8a3cdfff469e848e408e0a2c94671de5205e1da2
Bob Kline <bkline@rksystems.com>
2017-09-20 12:39:13 -0400

    Fix filtering and refine options (OCECDR-4300)

--- Build/check-build.py
+++ Build/check-build.py
@@ -3,24 +3,38 @@
 """
 Compare a CDR build package with the live file system.
 
+OCECDR-4300
 """
 
 import argparse
 import os
+import re
 import subprocess
 import sys
 
 class Control:
     """
     Master driver with runtime configuration settings for processing.
+
+    Class values:
+      SCRIPTS - directory where the build scripts are stored
+      EXCLUDES - location of file identifying things we don't check
+      SKIP - directories we don't check with this tool
+      POPEN_OPTS - options for launching a sub process
+
+    Attributes:
+      drive - letter representing the disk volume for the CDR
+      opts - runtime control settings
     """
 
-    PIPE = subprocess.PIPE
-    STDOUT = subprocess.STDOUT
-    POPEN_OPTS = dict(shell=True, stdout=PIPE, stderr=PIPE)
     SCRIPTS = os.path.split(os.path.abspath(sys.argv[0]))[0]
     EXCLUDES = os.path.join(SCRIPTS, "diff.excludes")
     SKIP = "Schemas", "Emailers"
+    POPEN_OPTS = dict(
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
 
     def __init__(self):
         """
@@ -36,10 +50,31 @@ class Control:
 
         Break out of the outer loop after the first iteration,
         because diff takes care of the recursion.
+
+        We build up the arguments for the diff invocations,
+        adding two None placeholders for the source and target dirs.
         """
 
+        args = ["diff", "-r"]
+        if not self.opts.all:
+            args += ["-X", self.EXCLUDES]
+        if self.opts.brief:
+            args.append("-q")
+        if self.opts.ignore_space_change:
+            args.append("-b")
+        if self.opts.ignore_all_space:
+            args.append("-w")
+        if self.opts.ignore_case:
+            args.append("-i")
+        if self.opts.unified:
+            args.append("-u")
+        if self.opts.context:
+            args.append("-c")
+        args += [None, None]
         for path, dirs, files in os.walk(self.opts.build):
-            for directory in dirs:
+            #for directory in dirs:
+            while dirs:
+                directory = dirs.pop(0)
                 source = os.path.join(path, directory)
                 if directory in self.SKIP:
                     continue
@@ -47,25 +82,30 @@ class Control:
                     target = self.drive + r":\Inetpub"
                 else:
                     target = self.drive + ":\\cdr\\" + directory
-                args = ["diff", self.opts.diffopts, source, target]
-                if not self.opts.all:
-                    args += ["-X", self.EXCLUDES]
+                args[-2:] = source, target
                 p = subprocess.Popen(args, **self.POPEN_OPTS)
                 output, errout = p.communicate()
                 if errout:
                     raise Exception(errout)
-                for line in output.splitlines():
-                    if not self.skip_line(line):
-                        print(line)
-            break
+                sys.stdout.write(self.filtered(output))
+            #break
 
-    def skip_line(self, line):
-        line = line.replace("/", "\\").lower()
-        if r"only in d:\inetpub\wwwroot: " in line:
-            return True
-        if r"only in d:\inetpub\wwwroot\cgi-bin\scheduler: static" in line:
-            return True
-        return False
+    def filtered(self, output):
+        """
+        Don't clutter up the output yakking about all the dross in the
+        DEV web root (unless the user has asked for everything).
+        """
+
+        if self.opts.all:
+            return output
+        skip = (
+            "only in d:/inetpub/wwwroot: ",
+            "only in d:/inetpub/wwwroot/cgi-bin/scheduler: static"
+        )
+        for s in skip:
+            pattern = s.replace("/", r"[\\/]") + r"[^\n]*\n"
+            output = re.sub(pattern, "", output, flags=re.IGNORECASE)
+        return output
 
     def fetch_options(self):
         """
@@ -75,16 +115,34 @@ class Control:
         desc = "Compare a CDR release set to the live file system"
         parser = argparse.ArgumentParser(description=desc)
         parser.add_argument("build", help="location of build files")
-        parser.add_argument("--diffopts", "-o", default="-r")
-        parser.add_argument("--all", "-a", action="store_true")
+        parser.add_argument("--brief", "-q", action="store_true",
+                            help="only report which files differ")
+        parser.add_argument("--ignore-case", "-i", action="store_true",
+                            help="ignore case differences in file contents")
+        parser.add_argument("--ignore-space-change", "-b", action="store_true",
+                            help="ignore changes in the amount of white space")
+        parser.add_argument("--ignore-all-space", "-w", action="store_true",
+                            help="ignore all white space")
+        parser.add_argument("--all", "-a", action="store_true",
+                            help="don't avoid reporting expected differences")
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument("--context", "-c", action="store_true",
+                           help="output three lines of context on each side")
+        group.add_argument("--unified", "-u", action="store_true",
+                           help="generate a unified context diff")
         return parser.parse_args()
 
     @staticmethod
     def find_cdr_drive():
+        """
+        Find out which drive has the CDR installation.
+        """
+
         for drive in "DCEFGH":
             if os.path.isdir("{}:/cdr".format(drive)):
                 return drive
         return None
 
-Control().run()
-exit(0)
+if __name__ == "__main__":
+    "Top-level entry point."
+    Control().run()
