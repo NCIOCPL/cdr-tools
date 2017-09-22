4bf175c7a331e5ca2b5dd271b49664fd611240d2
Bob Kline <bkline@rksystems.com>
2017-09-22 14:13:50 -0400

    Support reindexing doctype on other tiers (OCECDR-4285)

--- DevTools/Utilities/ReindexByDocType.py
+++ DevTools/Utilities/ReindexByDocType.py
@@ -1,38 +1,24 @@
+#!/usr/bin/env python
 #----------------------------------------------------------
 # Reindex all documents of a given doctype.
 #----------------------------------------------------------
-import cdr, cdrdb, sys, time
+import argparse
+import cdr
+import cdrdb
 
-if len(sys.argv) < 4:
-    sys.stderr.write(\
-      "usage: ReindexByDocType uid pwd doctype {max-docs}\n")
-    sys.exit(1)
-uid	= sys.argv[1]
-pwd  	= sys.argv[2]
-docType = sys.argv[3]
-maxDocs = len(sys.argv) > 4 and ("TOP %s " % sys.argv[4]) or ""
-session = cdr.login(uid, pwd)
-conn    = cdrdb.connect('CdrGuest')
-cursor  = conn.cursor()
-
-cursor.execute("""\
-        SELECT %s d.id
-          FROM document d
-          JOIN doc_type t
-            ON t.id = d.doc_type
-           AND t.name = '%s'
-      ORDER BY d.id""" % (maxDocs, docType))
-rows = cursor.fetchall()
-print "reindexing %d documents" % len(rows)
-count = 0
-for row in rows:
-    print "reindexing CDR%010d" % row[0]
-    resp = cdr.reindex('guest', row[0])
+parser = argparse.ArgumentParser()
+parser.add_argument("doctype")
+parser.add_argument("--max-docs", type=int)
+parser.add_argument("--tier", default=cdr.DEFAULT_HOST)
+opts = parser.parse_args()
+query = cdrdb.Query("document d", "d.id")
+query.join("doc_type t", "t.id = d.doc_type")
+query.where(query.Condition("t.name", opts.doctype))
+if opts.max_docs:
+    query.limit(opts.max_docs)
+rows = query.order("d.id").execute().fetchall()
+print("reindexing {} documents".format(len(rows)))
+for doc_id, in rows:
+    print("reindexing CDR%010d" % doc_id)
+    resp = cdr.reindex("guest", doc_id, opts.tier)
     if resp: print resp
-
-    # Pause every 100 docs (to avoid swallowing the machine? swamping sshd?)
-    # We had a problem at one time that this fixed - though it's probably
-    #   not needed any more
-    count += 1
-    if count % 50 == 0:
-        time.sleep(1)
