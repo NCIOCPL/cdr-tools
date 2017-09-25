cc59b148dfec9bac9df3a5586c333905acac0ad6
Bob Kline <bkline@rksystems.com>
2017-09-25 14:09:27 -0400

    Remove obsolete files

--- DevTools/Utilities/getVersion.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-if %4. == . goto usage
-@python -c "import cdr; print cdr.getDoc(('%1','%2'), '%3', version='%4')"
-goto done
-:usage
-@echo usage: getVersion user-id password doc-id which-version
-:done
