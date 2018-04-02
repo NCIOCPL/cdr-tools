e63bdde33e3cfdfa3669b0cb7d04874fe16f10ef
Bob Kline <bkline@rksystems.com>
2018-04-02 19:49:04 -0400

    Suppress verbose output

--- Build/backupDeploy.cmd
+++ Build/backupDeploy.cmd
@@ -6,4 +6,4 @@ IF ".%1%." == ".." (
     EXIT /B 1
 )
 SET ZIPNAME=%1%
-zip -r %ZIPNAME% d:\Inetpub d:\cdr\Bin d:\cdr\ClientFiles d:\cdr\Database d:\cdr\lib d:\cdr\Mailers d:\cdr\Publishing d:\cdr\Utilities d:\cdr\Licensee d:\cdr\Scheduler d:\cdr\glossifier d:\cdr\Schemas d:\cdr\Filters d:\cdr\Build d:\cdr\api
+zip -r -q %ZIPNAME% d:\Inetpub d:\cdr\Bin d:\cdr\ClientFiles d:\cdr\Database d:\cdr\lib d:\cdr\Mailers d:\cdr\Publishing d:\cdr\Utilities d:\cdr\Licensee d:\cdr\Scheduler d:\cdr\glossifier d:\cdr\Schemas d:\cdr\Filters d:\cdr\Build d:\cdr\api
