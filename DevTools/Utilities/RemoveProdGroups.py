362864b7e8cf3dffa8bdccb4ccd2f58ee01f2947
Volker Englisch <venglisch@users.noreply.github.com>
2018-06-12 18:09:40 -0400

    Modifying script to run from workstation with tier as a parameter

--- DevTools/Utilities/RemoveProdGroups.py
+++ DevTools/Utilities/RemoveProdGroups.py
@@ -8,17 +8,37 @@
 # after a DB refresh has been performed.
 #
 #----------------------------------------------------------------------
+import argparse
+import getpass
 import cdr, sys
 
 LOGFILE = "RemoveProdGroups.log"
 LOGLEVEL = 1
 
+def create_parser():
+    """
+    Create the object which collects the run-time arguments.
+    """
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="""\
+This program needs to be run on the lower tiers after database refresh.
+Since the DB is refreshed with the data from PROD the script will update 
+the email notification groups and remove all users.""")
+    parser.add_argument("--runmode", "-r", choices=['live', 'test'], 
+                                           required=True)
+    parser.add_argument("--tier", "-t", choices=['PROD','STAGE','QA','DEV'])
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--session", "-s")
+    group.add_argument("--user", "-u")
+    return parser
+
 # ---------------------------------------------------------------------
 # Function to update the groups membership
 # ---------------------------------------------------------------------
-def updateGroups(uid, pwd, testMode):
+def updateGroups(session, testMode, tier):
     test = testMode
-    session = cdr.login(uid, pwd)
 
     # Groups to be reset
     # ------------------
@@ -26,23 +46,26 @@ def updateGroups(uid, pwd, testMode):
                "CTGov Duplicate Notification":    ['volker'],
                "CTGov Export Notification":       ['bkline', 'volker'],
                "CTGov Link Fix Notification":     ['bkline', 'volker'],
+               "GovDelivery ES Docs Notification":['operator', 'volker'],
+               "GovDelivery EN Docs Notification":['operator', 'volker'],
                "Hotfix Remove Notification":      ['operator', 'volker'],
                "ICRDB Statistics Notification":   ['operator', 'volker'],
                "Licensee Report Notification":    ['operator', 'volker'],
                "Nightly Publishing Notification": ['operator', 'volker'],
                "Operator Publishing Notification":['operator', 'volker'],
+               "Test Group Dada":                 ['volker'],
                "Test Publishing Notification":    ['operator', 'volker'],
                "VOL Notification":                ['operator', 'volker'],
-               "Test Group Dada": ['volker'],
                "Weekly Publishing Notification":  ['operator', 'volker']}
+               }
 
     ierror = 0
     for group_name in groups:
-        group = cdr.getGroup(session, group_name)
-
-        # If the group doesn't exist on this tier continue
-        # ------------------------------------------------
-        if not group:
+        try:
+            group = cdr.getGroup(session, group_name, tier=tier)
+        except:
+            # If the group doesn't exist on this tier continue
+            # ------------------------------------------------
             ierror += 1
             l.write("***** ERROR *****", stdout = True)
             l.write("Group not available on this tier!!!", 
@@ -62,7 +85,7 @@ def updateGroups(uid, pwd, testMode):
         if testMode:
             l.write("TESTMODE:  No update", stdout = True)
         else:
-            error = cdr.putGroup(session, group_name, group)
+            error = cdr.putGroup(session, group_name, group, tier=tier)
             l.write("%s: %s" % (group_name, error or "saved"), stdout = True)
         l.write("----", stdout = True)
 
@@ -73,33 +96,38 @@ def updateGroups(uid, pwd, testMode):
 # -----------------------------------------------------------------
 if __name__ == "__main__":
 
+    # Parse command-line options and arguments
+    # ----------------------------------------
+    parser = create_parser()
+    opts = parser.parse_args()
+
     # Open Log file and enter start message
     # -------------------------------------
     l = cdr.Log(LOGFILE)
     l.write('RemoveProdGroups - Started', stdout = True)
-    l.write('Arguments: %s' % sys.argv, stdout=True)
-
-    if len(sys.argv) != 4:
-        l.write("usage: RemoveProdGroups.py userId pw live|test")
-        sys.stderr.write("usage: RemoveProdGroups.py userId pw live|test\n")
-        sys.exit(1)
-
-    # Get args
-    # --------
-    uid, pwd, runMode = sys.argv[1:]
+    # l.write('Arguments: %s' % opts, stdout=True)
 
     # Live or test mode
     # -----------------
-    if runMode not in ("live", "test"):
-        sys.stderr.write('Specify "live" or "test" for run mode\n')
-        sys.exit(1)
-
-    if runMode == "test":
+    if opts.runmode == "test":
         testMode = True
     else:
         testMode = False
 
-    error_count = updateGroups(uid, pwd, testMode)
+    tier = opts.tier
+
+    # Log into the CDR on the target server.
+    # --------------------------------------
+    if opts.session:
+        session = opts.session
+    else:
+        password = getpass.getpass()
+        session = cdr.login(opts.user, password, tier=opts.tier)
+        error_message = cdr.checkErr(session)
+        if error_message:
+            parser.error(error_message)
+
+    error_count = updateGroups(session, testMode, tier)
 
     l.write('RemoveProdGroups - Finished', stdout = True)
     l.write('Missing groups: %d' % error_count, stdout = True)
